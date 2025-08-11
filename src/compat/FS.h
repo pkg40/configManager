f7fea9a1e2e61815d83fbb41e716027fbbb8092c
pkg40 <pkg40@yahoo.com>
2025-08-11 12:56:01 -0700

    Add FS.h stub for native build

---
+++ src/compat/FS.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifdef CONFIGMGR_NATIVE
+#include "native_arduino_compat.hpp"
+// Provide File typedef expected by code
+// Our native_arduino_compat.hpp already defines class File
+#endif
